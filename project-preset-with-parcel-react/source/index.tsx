d62b86da16b9f70d1aad76df914c063d63b02e6e
Junior Programmer <progphyton@gmail.com>
2023-08-24 11:09:29 +0300

    fix: React is declared but its value is never read

--- project-preset-with-parcel-react/source/index.tsx
+++ project-preset-with-parcel-react/source/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import { createRoot } from 'react-dom/client';
 
 import { container } from './constants/container';
-import { createRoot } from 'react-dom/client';
+
 import { App } from './components/app/app';
 
 const ROOT = createRoot(container as HTMLElement);
